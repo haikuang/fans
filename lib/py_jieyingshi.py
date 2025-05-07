1bae0fdc71e2c18d56ca7a13a9a96c13d1159816
roczeng <roczeng@msn.com>
2025-05-07 20:18:18 +0800

    update

--- lib/py_jieyingshi.py
+++ lib/py_jieyingshi.py
@@ -133,7 +133,7 @@ class Spider(Spider):
                 'vod_actor': i['vodActor'],
                 'vod_director': i['vodDirector'],
                 'vod_content': i['vodContent'],
-                'vod_play_from': '影界在线',
+                'vod_play_from': '分享者在线',
                 'vod_play_url': '#'.join(urls),
 
             }
